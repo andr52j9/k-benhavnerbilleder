170fd2ceb84f46c17a22d1c39d465bb17891f1eb
Your Name <youremail@yourdomain.comAndreaandr52j9@stud.kea.dk>
2022-08-25 15:29:15 +0200

    webp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    colors: {
+      primary: "#E9DAC1",
+      accent: "#002B5B",
+    },
+
+    extend: {
+      fontFamily: {
+        h1: "poppins,system-ui, sans-serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
