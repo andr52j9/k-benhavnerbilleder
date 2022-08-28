0be19cdb671b82656cc4402f80c7eb96ebd2a065
Your Name <youremail@yourdomain.comAndreaandr52j9@stud.kea.dk>
2022-08-28 18:52:51 +0200

    nyt

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+document.querySelector("#darkMode").addEventListener("click", addDark);
+
+function addDark() {
+  document.documentElement.classList.add("dark");
+}
+
+document.querySelector("#lightMode").addEventListener("click", removeDark);
+
+function removeDark() {
+  document.documentElement.classList.remove("dark");
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,17 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
-
+  darkMode: "class",
   theme: {
-    colors: {
-      primary: "#E9DAC1",
-      accent: "#002B5B",
-    },
-
     extend: {
-      fontFamily: {
-        h1: "poppins,system-ui, sans-serif",
-      },
+      // colors: {
+      //   primary: "#E9DAC1",
+      //   accent: "#002B5B",
+      // },
+      // fontFamily: {
+      //   h1: "'Rye', cursive",
+      //   h2: "font-family: 'Lora', serif;",
+      // },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
